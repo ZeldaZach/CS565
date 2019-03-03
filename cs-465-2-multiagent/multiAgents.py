ca485af68649a24046da7775e0c5fe71abcb1348
Zach Halpern <ZaHalpern+github@gmail.com>
2019-03-03 00:02:46 -0500

    Alpha Agent
    Signed-off-by: Zach Halpern <ZaHalpern+github@gmail.com>

--- cs-465-2-multiagent/multiAgents.py
+++ cs-465-2-multiagent/multiAgents.py
@@ -227,15 +227,120 @@ class MinimaxAgent(MultiAgentSearchAgent):
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
     """
-      Your minimax agent with alpha-beta pruning (question 3)
+    Your minimax agent with alpha-beta pruning (question 3)
     """
 
-    def getAction(self, gameState):
+    def getAction(self, game_state):
         """
-          Returns the minimax action using self.depth and self.evaluationFunction
+        Get the next best action to take
+        :param game_state: Current game state
+        :return: Best action to take, based on the state
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # Start us with pacman in the current state
+        return self.max_value(game_state, -1 * sys.maxint, sys.maxint, 0, 0)[
+            "direction"
+        ]
+
+    def max_value(self, game_state, alpha, beta, state_depth, agent_number):
+        """
+        With Pacman, you'll want the best value possible from the tree.
+        This will calculate it and give you back the dictionary.
+        :param game_state: Current game state
+        :param alpha: Help break ties on the max
+        :param beta: Help break ties on the min
+        :param state_depth: How far in the tree you are
+        :param agent_number: Which agent is this? (Better be Pacman = 0)
+        :return: Best action with cost, as dictionary
+        """
+
+        # If we're at an end point, return out
+        if game_state.isWin() or game_state.isLose() or state_depth == self.depth:
+            return {
+                "cost": self.evaluationFunction(game_state),
+                "direction": Directions.STOP,
+            }
+
+        # Holders to get the largest options while iterating
+        best_cost = -1 * sys.maxint
+        best_action = Directions.STOP
+
+        legal_actions = game_state.getLegalActions(agent_number)
+        for action in legal_actions:
+            # Determine the worst of the ghosts and see if greater than current pacman
+            next_state = game_state.generateSuccessor(agent_number, action)
+            next_result = self.min_value(
+                next_state, alpha, beta, state_depth, agent_number + 1
+            )
+
+            if next_result["cost"] > best_cost:
+                best_cost = next_result["cost"]
+                best_action = action
+
+            # If we've beat our beta, lets escape
+            if best_cost > beta:
+                break
+
+            alpha = max(alpha, best_cost)
+
+        # We didn't find a better match
+        if best_cost == -1 * sys.maxint:
+            best_cost = self.evaluationFunction(game_state)
+            best_action = Directions.STOP
+
+        return {"cost": best_cost, "direction": best_action}
+
+    def min_value(self, game_state, alpha, beta, state_depth, agent_number):
+        """
+        With the ghosts, you'll want the worst value possible from the tree.
+        This will calculate it and give back a dictionary.
+        :param game_state: Current game state
+        :param alpha: Help break ties on the max
+        :param beta: Help break ties on the min
+        :param state_depth: How far in the tree
+        :param agent_number: Which ghost (better not be 0)
+        :return: Worst action with cost, as dictionary
+        """
+        # If we're at an end point, return out
+        if game_state.isWin() or game_state.isLose() or state_depth >= self.depth:
+            return {
+                "cost": self.evaluationFunction(game_state),
+                "direction": Directions.STOP,
+            }
+
+        # Holders to get the smallest options while iterating
+        best_cost = sys.maxint
+        best_action = Directions.STOP
+
+        legal_actions = game_state.getLegalActions(agent_number)
+        for action in legal_actions:
+            next_state = game_state.generateSuccessor(agent_number, action)
+
+            # Determine what our next action should be
+            # Do we depth or breadth
+            next_agent = (agent_number + 1) % game_state.getNumAgents()
+            plus_state = 1 if not next_agent else 0
+            max_pac_min_ghost = self.max_value if not next_agent else self.min_value
+
+            next_action = max_pac_min_ghost(
+                next_state, alpha, beta, state_depth + plus_state, next_agent
+            )
+
+            if next_action["cost"] < best_cost:
+                best_cost = next_action["cost"]
+                best_action = action
+
+            # If we lost to our alpha, lets escape
+            if best_cost < alpha:
+                break
+
+            beta = min(beta, best_cost)
+
+        # No better matches found
+        if best_cost == sys.maxint:
+            best_cost = self.evaluationFunction(game_state)
+            best_action = Directions.STOP
+
+        return {"cost": best_cost, "direction": best_action}
 
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
