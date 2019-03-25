009873da1224fa4c2be7113f6e72ae2e0ef26bd6
Zach Halpern <ZaHalpern+github@gmail.com>
2019-03-25 17:29:14 -0400

    Question 2/3

--- cs465-3-reinforcement/analysis.py
+++ cs465-3-reinforcement/analysis.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -19,45 +19,52 @@
 # Set the given parameters to obtain the specified policies through
 # value iteration.
 
+
 def question2():
-    answer_discount = 0.9
-    answerNoise = 0.2
-    return answer_discount, answerNoise
+    answerDiscount = 0.9
+    answerNoise = 0.005
+    return answerDiscount, answerNoise
+
 
 def question3a():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 0.6
+    answerNoise = 0.5
+    answerLivingReward = -4
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
+
 
 def question3b():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 0.6
+    answerNoise = 0.3
+    answerLivingReward = -1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
+
 
 def question3c():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 0.9
+    answerNoise = 0.005
+    answerLivingReward = 0.0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
+
 
 def question3d():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 0.9
+    answerNoise = 0.4
+    answerLivingReward = 0.0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
+
 def question3e():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 1.0
+    answerNoise = 0.005
+    answerLivingReward = 1.0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
+
 
 def question6():
     answerEpsilon = None
@@ -65,9 +72,11 @@ def question6():
     return answerEpsilon, answerLearningRate
     # If not possible, return 'NOT POSSIBLE'
 
-if __name__ == '__main__':
-    print('Answers to analysis questions:')
+
+if __name__ == "__main__":
+    print("Answers to analysis questions:")
     import analysis
-    for q in [q for q in dir(analysis) if q.startswith('question')]:
+
+    for q in [q for q in dir(analysis) if q.startswith("question")]:
         response = getattr(analysis, q)()
-        print('  Question %s:\t%s' % (q, str(response)))
+        print("  Question %s:\t%s" % (q, str(response)))
